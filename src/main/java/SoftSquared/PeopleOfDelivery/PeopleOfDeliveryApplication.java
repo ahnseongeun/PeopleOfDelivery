be13ce5626793d937deebae8688c1129d2dc3c26
ahnseongeun <ast3138@naver.com>
2021-01-27 11:51:13 +0900

    1월 26일 주문 내역 상세 조회 API 개발 완료

--- src/main/java/SoftSquared/PeopleOfDelivery/PeopleOfDeliveryApplication.java
+++ src/main/java/SoftSquared/PeopleOfDelivery/PeopleOfDeliveryApplication.java
@@ -1,6 +1,9 @@
 package SoftSquared.PeopleOfDelivery;
 
-import SoftSquared.PeopleOfDelivery.domain.order.Orders;
+import SoftSquared.PeopleOfDelivery.domain.coupon.Coupon;
+import SoftSquared.PeopleOfDelivery.domain.coupon.CouponRepository;
+import SoftSquared.PeopleOfDelivery.domain.menu.Menu;
+import SoftSquared.PeopleOfDelivery.domain.menu.MenuRepository;
 import SoftSquared.PeopleOfDelivery.domain.order.OrdersRepository;
 import SoftSquared.PeopleOfDelivery.domain.store.Store;
 import SoftSquared.PeopleOfDelivery.domain.store.StoreRepository;
@@ -16,13 +19,19 @@ public class PeopleOfDeliveryApplication implements CommandLineRunner{
 	private final UserRepository userRepository;
 	private final StoreRepository storeRepository;
 	private final OrdersRepository ordersRepository;
+	private final CouponRepository couponRepository;
+	private final MenuRepository menuRepository;
 
 	public PeopleOfDeliveryApplication(UserRepository userRepository,
 									   StoreRepository storeRepository,
-									   OrdersRepository ordersRepository) {
+									   OrdersRepository ordersRepository,
+									   CouponRepository couponRepository,
+									   MenuRepository menuRepository) {
 		this.userRepository = userRepository;
 		this.storeRepository = storeRepository;
 		this.ordersRepository = ordersRepository;
+		this.couponRepository = couponRepository;
+		this.menuRepository = menuRepository;
 	}
 
 	public static void main(String[] args) {
@@ -32,8 +41,15 @@ public class PeopleOfDeliveryApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception{
-		userRepository.save(new User("testName1","test1@naver.com","123321","010-1111-2222"
+		User user = userRepository.save(new User("testName1","test1@naver.com","123321","010-1111-2222"
 				,"서울시송파구잠실6동", 1,1,"1995-03-10",1));
+
+		couponRepository.save(Coupon.builder()
+				.user(user)
+				.coupon1000(5)
+				.coupon3000(5)
+				.coupon5000(0)
+				.build());
 
 		/**
 		 * 주문 많은순
@@ -41,7 +57,7 @@ public class PeopleOfDeliveryApplication implements CommandLineRunner{
 		 * order가 1개, 2개
 		 * return은 2개, 개순
 		 */
-		storeRepository.save(Store.builder()
+	Store store =	storeRepository.save(Store.builder()
 				.id(1L)
 				.name("testStore1")
 				.phoneNumber("010-1111-1111")
@@ -66,41 +82,38 @@ public class PeopleOfDeliveryApplication implements CommandLineRunner{
 				.status(1)
 				.build());
 
-//		ordersRepository.save(Orders.builder()
-//				.id(1L)
-//				.requestContent("test1")
-//				.user(userRepository.findById(1L).orElseGet(null))
-//				.store(storeRepository.findById(1L).orElseGet(null))
-//				.status(1)
-//				.orderDetails(null)
-//				.reviews(null)
-//				.payment(null)
-//				.address("test1")
-//				.build());
-//
-//		ordersRepository.save(Orders.builder()
-//				.id(2L)
-//				.requestContent("test12")
-//				.user(userRepository.findById(1L).orElseGet(null))
-//				.store(storeRepository.findById(2L).orElseGet(null))
-//				.status(1)
-//				.orderDetails(null)
-//				.reviews(null)
-//				.payment(null)
-//				.address("test2")
-//				.build());
-//
-//		ordersRepository.save(Orders.builder()
-//				.id(3L)
-//				.requestContent("test3")
-//				.user(userRepository.findById(1L).orElseGet(null))
-//				.store(storeRepository.findById(2L).orElseGet(null))
-//				.status(1)
-//				.orderDetails(null)
-//				.reviews(null)
-//				.payment(null)
-//				.address("test3")
-//				.build());
+		menuRepository.save(Menu.builder()
+				.store(store)
+				.popularCheck(1)
+				.description("test1")
+				.price(30000)
+				.imageURL("default")
+				.status(1)
+				.name("test1")
+				.imageStatus(2)
+				.build());
+
+		menuRepository.save(Menu.builder()
+				.store(store)
+				.popularCheck(1)
+				.description("test2")
+				.price(20000)
+				.imageURL("default")
+				.status(1)
+				.name("test2")
+				.imageStatus(2)
+				.build());
+
+		menuRepository.save(Menu.builder()
+				.store(store)
+				.popularCheck(1)
+				.description("test3")
+				.price(10000)
+				.imageURL("default")
+				.status(1)
+				.name("test3")
+				.imageStatus(2)
+				.build());
 	}
 
 }
